8396948f9f8613d7b48d60fdf061ed98cbb4b785
Fex-zi <fexzi87@gmail.com>
2023-10-10 18:42:49 -0600

    USERS

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,26 @@
 const router = require('express').Router();
 router.use('/', require('./swagger'));
+const UserController = require('../controllers/users');
 
 // PASTE RIGHT HERE YOUR ROUTES
 
 router.use('/reviews', require('./reviews'));
+
+//USER ROUTES
+// GET all users
+router.get('/', UserController.getAllUsers);
+
+// GET a single user by ID
+router.get('/:id', UserController.getUserById);
+
+// POST a new user
+router.post('/', UserController.createUser);
+
+// PUT update a user
+router.put('/:id', UserController.updateUser);
+
+// DELETE a user
+router.delete('/:id', UserController.deleteUser);
+
 
 module.exports = router;
